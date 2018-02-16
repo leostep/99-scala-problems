38f586cd326e4998c17b9e8e52b13a933a39ade7
Leonid Stepanenko <leonid.step93@gmail.com>
2018-02-16 21:34:08 +0200

    Problem 4 - list size

---
+++ src/main/scala/P04.scala
@@ -0,0 +1,16 @@
+import scala.annotation.tailrec
+
+object P04 {
+
+  def lengthBuiltin[A](ls: List[A]): Long = ls.length
+
+  def length[A](ls: List[A]): Long = {
+    @tailrec def size(n: Long, xs: List[A]): Long = xs match {
+      case Nil => n
+      case _ :: tail => size(n + 1, tail)
+    }
+
+    size(0, ls)
+  }
+
+}
