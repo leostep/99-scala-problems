b3459b58dcadb536cb70ed168e888debd16a0f42
Leonid Stepanenko <leonid.step93@gmail.com>
2018-02-17 12:36:47 +0200

    Problem 5 - reverse a list

---
+++ src/main/scala/P05.scala
@@ -0,0 +1,21 @@
+object P05 {
+
+  def reverseBuiltin[A](ls: List[A]): List[A] = ls.reverse
+
+  def reverseRec[A](ls: List[A]): List[A] = ls match {
+    case x :: xs => reverseRec(xs) :+ x
+    case _ => ls
+  }
+
+  def reverseTailRec[A](list: List[A]): List[A] = {
+    def revRec(ls: List[A], rs: List[A]): List[A] = rs match {
+      case Nil => ls
+      case x :: xs => revRec(x :: ls, xs)
+    }
+
+    revRec(List(), list)
+  }
+
+  def reverseFunctional[A](list: List[A]): List[A] = list.foldLeft(List[A]())((xs, e) => e :: xs)
+
+}
