5319923538b083aa45ec4c386ddfeee1b4e733e1
Leonid Stepanenko <leonid.step93@gmail.com>
2018-11-24 15:01:32 +0100

    Added @tailrec annotation to problem 5

--- src/main/scala/lists/P05Reverse.scala
+++ src/main/scala/lists/P05Reverse.scala
@@ -1,5 +1,7 @@
 package lists
 
+import scala.annotation.tailrec
+
 object P05Reverse {
 
   def reverseBuiltin[A](ls: List[A]): List[A] = ls.reverse
@@ -10,7 +12,7 @@ object P05Reverse {
   }
 
   def reverseTailRec[A](list: List[A]): List[A] = {
-    def revRec(ls: List[A], rs: List[A]): List[A] = rs match {
+    @tailrec def revRec(ls: List[A], rs: List[A]): List[A] = rs match {
       case Nil => ls
       case x :: xs => revRec(x :: ls, xs)
     }
